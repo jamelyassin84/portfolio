1f98cba1a179e573ac25aa929e336fe1865823ae
Jamel Eid Yassin <42769414+jamelyassin84@users.noreply.github.com>
2023-06-28 14:25:08 +0400

    Changes

--- src/app/app-core/navigations/navigation.ts
+++ src/app/app-core/navigations/navigation.ts
@@ -1,12 +1,15 @@
 export const NAVIGATION = [
     {
         title: 'ABOUT',
+        div: 'div_about',
     },
     {
         title: 'WORK EXPERIENCES',
+        div: 'div_workExperiences',
     },
 
     {
         title: 'PROJECTS AND MORE',
+        div: 'div_projectsAndMore',
     },
 ]
--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core'
+import {Component, HostListener} from '@angular/core'
 import {NAVIGATION} from 'app/app-core/navigations/navigation'
 
 @Component({
@@ -6,6 +6,39 @@ import {NAVIGATION} from 'app/app-core/navigations/navigation'
     templateUrl: './sidebar.component.html',
 })
 export class SidebarComponent {
+    @HostListener('window:scroll', ['$event'])
+    onScroll() {
+     const divs = document.querySelectorAll('[id^="div_"]')
+        let currentDiv: string | undefined
+
+        console.log(divs)
+
+        for (let i = 0; i < divs.length; i++) {
+            const div = divs[i] as HTMLElement
+            const rect = div.getBoundingClientRect()
+
+            if (rect.top >= 0 && rect.bottom <= window.innerHeight) {
+                currentDiv = div.id
+                break
+            }
+        }
+
+        if (currentDiv) {
+            this.currentMenu = this.getMenuTitleFromDivId(currentDiv)
+        }
+    }
     readonly NAVIGATION = NAVIGATION
     currentMenu = 'ABOUT'
+
+    scrollToDiv(divId: string) {
+        const element = document.querySelector(`#${divId}`)
+        if (element) {
+            element.scrollIntoView({behavior: 'smooth', block: 'start'})
+        }
+    }
+
+    private getMenuTitleFromDivId(divId: string): string {
+        const menu = this.NAVIGATION.find((item) => item.div === divId)
+        return menu ? menu.title : ''
+    }
 }
